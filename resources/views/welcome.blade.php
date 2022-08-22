4c511226ea4625f6ca61f9d5d7e30463e6d4ab1c
Hernando Ortega <51216975+NanoLaravel@users.noreply.github.com>
2022-08-22 15:30:51 -0500

    inicio del proyecto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>chinacotatravel.com</title>
+
+    @vite('resources/css/app.css')
+</head>
+<body> 
+  
+  <div class="container">
+    <div>
+      <h1>
+       CHINACOTATRAVEL.COM
+      </h1>
+      <H6>todo en un solo lugar</H6>
+    </div>
+    <ul class="">
+      <li class="">
+          <a class="" href="#">¿DONDE ALOJARSE</a>
+      </li>
+      <li class="">
+          <a class="" href="#">¿DONDE COMER?</a>
+      </li>
+      <li class="">
+          <a class="" href="#">¿QUE HACER?</a>
+      </li>
+      <li class="">
+          <a class="" href="#">BLOG</a>
+      </li>
+      <li class="">
+          <a class="" href="#">LOGIN</a>
+      </li>
+      <li class="">
+          <a class="" href="#">CLIMA EN CHINACOTA</a>
+      </li>
+  </ul>
+
+  
+    <div class="">
+        <img  src="{{URL::asset('assets/hotel/hot1recortado2.jpg')}}" alt="restaurante 4">
+        <h6 class="">¿DONDE ALOJARSE?</h6>
+    </div>
+    <div class="">
+      <img src="assets/hotel/hot1recortado.jpg" alt="restaurante 2 ">
+        <h6 class="">¿DONDE COMER?</h6>
+    </div>
+
+
+    </div>      
+        
+
+            
+        
+   
+   
+
+</body>
+</html>
